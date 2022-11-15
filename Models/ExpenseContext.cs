f73290885411ef27d5b2ba1847662e46917d797d
oana oprea <oanamaria.oprea@yahoo.com>
2022-11-15 19:20:09 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Oana_Oprea_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
